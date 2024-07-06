b0d85f40b92ba1648ceff14681e93a1eb63207a4
YinLong <2804566988@qq.com>
2024-07-06 18:17:45 +0800

    Q28

---
+++ src/Solution/Q28.java
@@ -0,0 +1,27 @@
+package Solution;
+
+public class Q28 {
+    class Solution {
+        public int strStr(String haystack, String needle) {
+            int needleIndex = 0;
+            if(haystack.length() < needle.length()){
+                return -1;
+            }
+            for(int i = 0;i < haystack.length();i++){
+                int temp = i;
+                while(haystack.charAt(temp) == needle.charAt(needleIndex)){
+                    temp++;
+                    needleIndex++;
+                    if(needleIndex == needle.length()) {
+                        return i;
+                    }
+                    if(temp >= haystack.length()){
+                        return -1;
+                    }
+                }
+                needleIndex = 0;
+            }
+            return -1;
+        }
+    }
+}
